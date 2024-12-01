fb787c9afc256dc092cb510899b1764e5955e145
ThinhT1 <tangthinh1304@gmail.com>
2024-12-01 20:09:21 +0700

    project3

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=thinht
+export POSTGRES_PASSWORD=abcde12345
+export POSTGRES_HOST=testproject3.cc6f9vidwjib.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=thinht1-032837203237-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
